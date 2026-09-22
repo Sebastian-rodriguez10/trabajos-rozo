541fc5889ff3f63545e0fd2b23de71770bcc9a93
sebastian rodriguez <juanserodriguez711@gmail.com>
2026-09-21 20:40:39 -0500

    fix: convertir pokedex-lite a carpeta normal

---
+++ pokedex-lite/src/app/_layout.tsx
@@ -0,0 +1,13 @@
+// app/_layout.tsx
+import { Stack } from "expo-router";
+
+const RootLayout = () => {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: "Pokedex Lite" }} />
+      <Stack.Screen name="pokemon/[name]" options={{ title: "Detalle" }} />
+    </Stack>
+  );
+};
+
+export default RootLayout;
